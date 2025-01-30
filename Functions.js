2612bddcf85666d94a816ad923d8246d23dbe060
Alvin Rubagumya <134910101+Alvin-256@users.noreply.github.com>
2025-01-30 11:20:47 +0300

    Add files via upload

---
+++ Functions.js
@@ -0,0 +1,18 @@
+//returning Functions
+
+// function name(){
+//     return;
+// }
+
+// const name= (param)=>{
+//       return param;
+// }
+
+
+const greeting= (person)=> {
+    console.log("Hello", person);
+}
+
+greeting("Alvin");
+
+//
---
+++ controlflows.js
@@ -0,0 +1,33 @@
+// loops
+// for looops
+
+// for(let i=1; i<=100; i++){
+//     console.log(i);
+// }
+
+// ages=[ 10, 22, 56, 60]
+// for (let i in ages){
+//     console.log(ages[i]);
+
+
+// }
+
+/// switch
+switch("Friday"){
+    case "Wednesday":
+    break; 
+
+    case "Monday":
+    break;
+    
+    case "Tuesday":
+    break;
+
+    case "Friday":
+        console.log("Today is Friday!");
+    break;
+
+    default: 
+    console.log("invalid day of the week");
+
+}
